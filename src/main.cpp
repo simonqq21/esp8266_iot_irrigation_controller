63ddfab3c91f2d1e0095229885c419ea078b8476
Simon Que <ques86225@gmail.com>
2023-12-16 12:18:06 +0800

    put pin definitions and variables described the format of the configuration variables in EEPROM and how to save and load data from the EEPROM

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,76 @@
 #include <ESPAsyncWebServer.h>
 #include <LittleFS.h> 
 #include <EEPROM.h>
+#include <RTClib.h>
 
 // pins 
-#define RELAY_PIN // 
-#define BUTTON_PIN //  
-#define LED_PIN //
+#define RELAY_PIN 14 // D5 
+#define BUTTON_PIN 0 // D3
+#define LED_PIN 2 // D4
+
+// status variables 
+byte hourBits[3];
+int duration = 0;
+
+// configuration variables 
+/*  The configuration variables are the ff:
+- a list of three bytes where the 24 hours per day are represented. The data is 
+  stored in little endian order, so the organization of 24 hours into three bytes
+  will be arranged as below:
+
+  address |       00                    01                       02
+  hours   |7 6 5 4 3 2 1 0 | 15 14 13 12 11 10 9 8 | 23 22 21 20 19 18 17 16
+  
+  so if a bit is high, the relay will close contacts for the set duration at the start
+  of that hour before opening contacts for the rest of the hour, but if that bit is 
+  low, then the relay will remain open contact for that hour.
+
+  - a single integer used to store the number of seconds the relay will be closed 
+  at the start of every hour the irrigation is set.
+
+  eg. if the relay must close at 7AM and remain open the rest of the time,
+  7 / 8 = 0, byte 0.  0000 0111 >> 3 = 0
+  7 % 8 = 7, 7th bit. 0000 0111 & 
+  change byte 0, 7th bit to 1. 
+      xxxxxxxx
+  or  10000000 (1 << 7)
+  ------------
+  =   1xxxxxxx
+
+  if changing a bit to 0, 
+      xxxxxxxx
+  and 01111111 (!(1 << 7))
+  ------------
+  =   1xxxxxxx
+
+  TLDR: To switch a bit on, use bitwise or with a left shift, to switch a bit off, 
+  use bitwise and with the inverse of a left shift. */
+
+
+// async web server
+AsyncWebServer server(80); 
+AsyncWebSocket ws("/ws"); 
+StaticJsonDocument<100> inputDoc;
+StaticJsonDocument<100> outputDoc;
+char strData[140];
 
 // wifi credentials
 #define LOCAL_SSID "QUE-STARLINK"
 #define LOCAL_PASS "password"
+//static IP address configuration 
+IPAddress local_IP(192,168,5,75);
+IPAddress gateway(192,168,5,1);
+IPAddress subnet(255,255,255,0);
+//IPAddress primaryDNS(8,8,8,8);
+//IPAddress secondaryDNS(8,8,4,4);
+#define APMODE true
 
 //for littlefs
 File indexPage;  
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+
 }
 
 void loop() {
