c9a9b54a62e97be527898dc8e79167493f653256
Simon Que <ques86225@gmail.com>
2024-02-07 19:47:07 +0800

    added in EEPROM related code

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,18 @@ byte hourBits[3];
 int duration = 0;
 bool relayClosed = false; 
 
+// EEPROM 
+#define STARTING_ADDR 0x0
+
+// data structure for storing config in EEPROM 
+typedef struct {
+  byte schedule[3];
+  short duration; 
+} timingconfig;
+// 
+timingconfig tC;
+bool* hours;
+
 // RTC 
 RTC_DS1307 rtc; 
 DateTime dtnow; 
@@ -29,6 +41,7 @@ DateTime dtnow;
 const long UTCOffsetInSeconds = 28800;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "ntp.pagasa.dost.gov.ph"); 
+
 
 // // string array of days of the week
 // String daysOfTheWeek[7] = {
@@ -44,33 +57,40 @@ NTPClient timeClient(ntpUDP, "ntp.pagasa.dost.gov.ph");
 // configuration variables 
 /*  The configuration variables are the ff:
 - a list of three bytes where the 24 hours per day are represented. The data is 
-  stored in little endian order, so the organization of 24 hours into three bytes
-  will be arranged as below:
+stored in little endian order, so the organization of 24 hours into three bytes
+will be arranged as below:
 
-  address |       00                    01                       02
-  hours   | 7 6 5 4 3 2 1 0 | 15 14 13 12 11 10 9 8 | 23 22 21 20 19 18 17 16
-  
-  so if a bit is high, the relay will close contacts for the set duration at the start
-  of that hour before opening contacts for the rest of the hour, but if that bit is 
-  low, then the relay will remain open contact for that hour.
+address |       00                    01                       02
+hours   | 7 6 5 4 3 2 1 0 | 15 14 13 12 11 10 9 8 | 23 22 21 20 19 18 17 16
 
-  eg. if the relay must close at 7AM and remain open the rest of the time,
-  7 / 8 = 0, byte 0.  0000 0111 >> 3 = 0
-  7 % 8 = 7, 7th bit. 0000 0111 & 
-  change byte 0, 7th bit to 1. 
-      xxxxxxxx
-  or  10000000 (1 << 7)
-  ------------
-  =   1xxxxxxx
+so if a bit is high, the relay will close contacts for the set duration at the start
+of that hour before opening contacts for the rest of the hour, but if that bit is 
+low, then the relay will remain open contact for that hour.
 
-  if changing a bit to 0, 
-      xxxxxxxx
-  and 01111111 (!(1 << 7))
-  ------------
-  =   1xxxxxxx
+eg. if the relay must close at 7AM and remain open the rest of the time,
+7 / 8 = 0, byte 0.  0000 0111 >> 3 = 0
+7 % 8 = 7, 7th bit. 0000 0111 & 
+change byte 0, 7th bit to 1. 
+    xxxxxxxx
+OR  10000000 (1 << 7)
+------------
+=   1xxxxxxx
 
-  TLDR: To switch a bit on, use bitwise or with a left shift, to switch a bit off, 
-  use bitwise and with the inverse of a left shift. */
+if changing a bit to 0, 
+    xxxxxxxx
+AND 01111111 (!(1 << 7))
+------------
+=   1xxxxxxx
+
+Perform modulo 8 by AND of the number and 00000111.
+
+TLDR: 
+  To get a certain bit, first integer divide the hour by 8 to get the nth
+  byte, bitwise shift right the nth byte by the modulo of the hour and 8, then
+  AND the result with 00000001 to get a 1 or a 0. 
+  To switch a bit on, use bitwise OR with a left shift, to switch a bit off, 
+use bitwise AND with the inverse of a left shift. 
+*/
 
 /* JSON formats: 
  - browser request status update from MCU 
@@ -135,25 +155,27 @@ void printNTPTime(NTPClient timeClient);
 void updateNTPTime();
 void adjustRTCWithNTP(NTPClient timeClient, RTC_DS1307 rtc);
 
-
-
-// void printTimingConfig(timingconfig tC);
-// void loadFromEEPROM(unsigned int addr, timingconfig* tC);
-// void saveToEEPROM(unsigned int addr, timingconfig tC);
-// bool checkHour(timingconfig tC, int hour);
-// bool* getActiveHours(timingconfig tC);
-// void setHour(timingconfig* tC, int hour, bool newState); 
-// void clearAllHours(timingconfig* tC); 
-// void setDuration(timingconfig* tC, int newDuration);
-
+void printTimingConfig(timingconfig tC);
+void loadFromEEPROM(unsigned int addr, timingconfig* tC);
+void saveToEEPROM(unsigned int addr, timingconfig tC);
+bool checkHour(timingconfig tC, int hour);
+bool* getActiveHours(timingconfig tC);
+void setHour(timingconfig* tC, int hour, bool newState); 
+void clearAllHours(timingconfig* tC); 
+void setDuration(timingconfig* tC, int newDuration);
 
 
 void setup() {
   Serial.begin(115200); 
-
-  // initialize EEPROM emulation
-  EEPROM.begin(10); 
   
+  // initialize the emulated EEPROM as large as needed
+  EEPROM.begin(sizeof(timingconfig));
+  
+  // load previous timing configuration from EEPROM if it exists
+  loadFromEEPROM(STARTING_ADDR, &tC);
+  Serial.println("previous tC loaded from EEPROM: ");
+  printTimingConfig(tC);
+
   // littleFS 
   if (!LittleFS.begin()) {
     Serial.println("An error occured while mounting LittleFS.");
@@ -168,12 +190,6 @@ void setup() {
     Serial.println("Couldn't find RTC.");
     while (1);
   }
-
-  /*
-  on startup, pull time from RTC.
-  when internet connected, update RTC with NTP. 
-  when internet not connected, get time from RTC. 
-  */
 
   Serial.print("Connecting to "); 
   Serial.println(LOCAL_SSID);
@@ -327,3 +343,84 @@ void initWebSocket() {
   server.addHandler(&ws);
 }
 
+void loadFromEEPROM(unsigned int addr, timingconfig* tC) {
+  EEPROM.get(addr, tC);
+}
+
+void saveToEEPROM(unsigned int addr, timingconfig tC) {
+  EEPROM.put(addr, tC);
+  EEPROM.commit();
+}
+
+void printTimingConfig(timingconfig tC) {
+  Serial.print("schedule bytes = ");
+  for (int i=0;i<3;i++) {
+    Serial.print(tC.schedule[i]);  
+    if (i<2)
+      Serial.print(", ");
+    else Serial.print(" ");
+  }
+  Serial.print("duration = ");
+  Serial.print(tC.duration);
+  Serial.println();
+}
+
+bool checkHour(timingconfig tC, int hour) {
+  bool status;
+  byte byteIndex = hour / 8; 
+  byte currByte = tC.schedule[byteIndex]; 
+  byte offset = hour % 8; 
+  currByte = currByte >> offset;
+  currByte = currByte & 1; 
+  status = (currByte) ? true: false;
+  return status;
+}
+
+bool* getActiveHours(timingconfig tC) {
+  static bool hours[24];
+  // reset the hours array
+  for (int h=0;h<24;h++) {
+    hours[h] = 0;
+  }
+  // check each bit in the schedule bytes then load it into the bool hours array 
+  for (int h=0;h<24;h++) { 
+    hours[h] = checkHour(tC, h);
+  }
+  return hours;
+}
+
+/* set the hour in the timing configuration to the specified state. 
+args:
+  tC - timingConfig object 
+  hour - hour of the day from 0-24 
+  newState - 0 for disable and 1 for enable 
+*/ 
+void setHour(timingconfig* tC, int hour, bool newState) {
+  int byteIndex = hour / 8;
+  int bitIndex = hour % 8; 
+  int mask = 1 << bitIndex; 
+  Serial.print("mask = ");
+  // if enabling the hour
+  if (newState) {
+    tC->schedule[byteIndex] = tC->schedule[byteIndex] | mask;
+  }
+  // else disabling the hour
+  else {
+    mask = ~mask;
+    tC->schedule[byteIndex] = tC->schedule[byteIndex] & mask;
+  }
+  Serial.println(mask);
+}
+
+// set the duration of the timing configuration 
+void setDuration(timingconfig* tC, int newDuration) {
+  tC->duration = newDuration;
+}
+
+// macro function to clear all hours in the schedule and reset interval to 0
+void clearAllHours(timingconfig* tC) {
+  setDuration(tC, 0);
+  for (int h=0; h<24; h++) {
+    setHour(tC, h, 0);
+  }
+}
